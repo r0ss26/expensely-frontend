0883a4df091eadda6fcb6d760157bbc10b9ec958
Ross Baker <rossbaker32@gmail.com>
2020-07-15 22:24:13 +1000

    Adds a request to the login form to get and store the jsonwebtoken

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,29 +1,50 @@
 import React, { useState } from "react";
+import axios from 'axios'
 
 const LoginSignup = () => {
-  const [formType, setFormType] = useState('signup');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleInput = (event, setInputState) => {
+    setInputState(event.target.value)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      email,
+      password
+    }
+
+    try {
+      const res = await axios.post('/auth/login', formData)
+      localStorage.setItem('jwt', res.data.token)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <div className="container">
 
-      <div class="row">
-        <form class="col s12">
-          <div class="row">
-            <div class="input-field col s12">
-              <input id="email" type="email" class="validate" />
-              <label for="email">Email</label>
+      <div className="row">
+        <form onSubmit={handleSubmit} className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input onChange={(event) => handleInput(event, setEmail)} value={email} id="email" type="email" className="validate" />
+              <label htmlFor="email">Email</label>
             </div>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <input id="password" type="password" class="validate" />
-              <label for="password">Password</label>
+          <div className="row">
+            <div className="input-field col s12">
+              <input onChange={(event) => handleInput(event, setPassword)} value={password} id="password" type="password" className="validate" />
+              <label htmlFor="password">Password</label>
             </div>
           </div>
+          <button className="btn waves-effect waves-light" name="action">Login
+            <i className="material-icons right">send</i>
+          </button>
         </form>
-        <button class="btn waves-effect waves-light" name="action">Login
-          <i class="material-icons right">send</i>
-        </button>
       </div>
     </div>
   )
