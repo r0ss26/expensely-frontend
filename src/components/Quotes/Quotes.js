30ea8e85cccbc7ed99864a33058369ec8be11393
Ross Baker <rossbaker32@gmail.com>
2020-07-28 10:45:21 +1000

    Use .then when fetching quotes

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -4,17 +4,21 @@ import axios from 'axios';
 const Quotes = () => {
   const [quotes, setQuotes] = useState(null);
 
-  useEffect(async () => {
-    const res = await axios.get('https://type.fit/api/quotes');
-    setQuotes(res.data);
+  useEffect(() => {
+    axios.get('https://type.fit/api/quotes').then((res) => setQuotes(res.data));
   }, []);
 
   return (
     <div class="row">
       <div class="col s12 m5">
         <div class="card-panel teal">
-          <span class="white-text">{quotes && quotes[Math.floor(Math.random() * quotes.length)].text}</span>
-            <div class="black-text"> {quotes && quotes[Math.floor(Math.random() * quotes.length)].author}</div>
+          <span class="white-text">
+            {quotes && quotes[Math.floor(Math.random() * quotes.length)].text}
+          </span>
+          <div class="black-text">
+            {' '}
+            {quotes && quotes[Math.floor(Math.random() * quotes.length)].author}
+          </div>
         </div>
       </div>
     </div>
