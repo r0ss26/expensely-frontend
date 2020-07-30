b0fb3108b629d26842e594019491a4ebd3f46c7a
lingyenlee <lingyenlee@gmail.com>
2020-07-30 10:46:12 +1000

    fix budget progress bar and add css

--- src/components/Charts/ProgressBar/ProgressBar.js
+++ src/components/Charts/ProgressBar/ProgressBar.js
@@ -3,13 +3,17 @@ import Filler from './Filler'
 import '../chartStyle.css'
 
 const ProgressBar = (props) => {
-   
+
     return (
         <div className='progress-bar' >
             <Filler items={props.items} />
             <div>
+                <h5 className="center">Your Budget Plan</h5>
+
                 <p>Your {props.items.duration} budget for {props.items.name} is ${props.items.budgetAmount}</p>
-                <p>You have spent ${props.items.spentAmount} or {`${props.items.percentSpent * 100}%`} of your budget. Amount left to spent ${props.items.budgetLeft}</p>
+                <p>You have spent {`${props.items.percentSpent.toFixed(1) * 100}%`} of your budget. Amount left to spent ${props.items.budgetLeft}</p>
+
+
             </div>
         </div>
 
